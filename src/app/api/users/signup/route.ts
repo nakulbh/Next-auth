b601c774abcd2552ec83e97eeebf3ecf2f8319d6
nakulbh <nakulbhardwaj37@gmail.com>
2024-05-27 01:37:58 +0530

    singup Routes

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,11 +1,29 @@
+import { error } from "console";
 import prisma from "../../../../dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
 export async function POST(request: NextRequest) {
   try {
-    const reqBody = request.json()
-    const{username, email, password} = reqBody
-    await 
+    const reqBody = request.json();
+
+    const { username, email, password } = reqBody;
+    //validation using zod
+
+    console.log(reqBody);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exits" },
+        { status: 400 }
+      );
+    }
   } catch (error: any) {
     return NextResponse.json(
       { error: error.message },
